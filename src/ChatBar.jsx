20fbf881382a453835ff69bbc108351313a0f764
Bonita Choi <bonitac93@gmail.com>
2019-04-10 22:08:56 +0000

    have message printing on server-side console

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,11 +1,44 @@
 import React, {Component} from 'react';
 
 class ChatBar extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      currentUser: this.props.currentUser.name,
+      content: ''
+    }
+  }
+  componentDidMount(){
+  }
+  
   render() {
+    console.log("inchatbar")
+    const socket = new WebSocket("ws://localhost:3001");
+
+    const handleKeyPress = (event) => {
+      if(event.key === 'Enter'){
+        // const id = 4
+        this.setState({content:event.target.value, username: this.state.currentUser})
+        this.props.addNewMessage(this.state)
+      }
+    }
+
+    const wsKeyUp = (event) => {
+      if (event.key === 'Enter'){
+        const msg = {content:event.target.value, username: this.state.currentUser};
+        socket.send(JSON.stringify(msg))
+        event.target.value = ""
+      }
+    }
+
     return (
       <footer className="chatbar">
-      <input className="chatbar-username" placeholder="Your Name (Optional)" />
-      <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+      <input className="chatbar-username"
+      placeholder={this.props.currentUser?this.props.currentUser.name : "Your Name (Optional)"} />
+      <input className="chatbar-message"
+      content = {this.state.content}
+      placeholder="Enter message here"
+      onKeyPress = {wsKeyUp}/>
       </footer>
     );
   }
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -2,11 +2,22 @@ import React, {Component} from 'react';
 import Message from './Message.jsx';
 
 class MessageList extends Component {
+  onNewPost (content){
+    messages.push(content)
+  }
+
   render() {
+    const messages = this.props.messages.map(message => {
+      return <Message
+      key = {message.id}
+      username = {message.username}
+      content = {message.content}
+      />
+    })
     return (
       <main className="messages">
       <script src = {Message}></script>
-      <Message />
+      {messages}
       <div className="message system">
         Anonymous1 changed their name to nomnom.
       </div>
